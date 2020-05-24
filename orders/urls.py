c4610a9eefeed8761aee80daf33eac62249726bc
Kwok cheong <wongkwokcheong@hotmail.com>
2020-05-25 05:10:30 +0800

    Added contexts.py to show number of items in shopping cart, added routes to nav bar

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 import orders.views
 
 urlpatterns = [
-    path('', orders.views.index),
+    path('', orders.views.index, name="show_order_route"),
     path('create/<product_id>', orders.views.create_order, name="create_order_route"),
 ]
