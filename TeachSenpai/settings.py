a808a1b0c105e4c592daebd812c255d715a224dd
Kwok cheong <wongkwokcheong@hotmail.com>
2020-06-05 15:15:18 +0800

    added *

--- TeachSenpai/settings.py
+++ TeachSenpai/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["teachsenpai.herokuapp.com/"]
+ALLOWED_HOSTS = ["teachsenpai.herokuapp.com/","*"]
 
 
 # Application definition
